88f1b02cc6a6279bfe9da7021efa1e9767b17040
Kevin Rivera <shimpo11.kr@gmail.com>
2023-02-13 12:00:00 -0500

    Añadir módulo de paginación y formulario reactivos

--- ejercicio01/src/app/app.module.ts
+++ ejercicio01/src/app/app.module.ts
@@ -3,19 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 
+import { NgxPaginationModule } from 'ngx-pagination';
 import { StaticModule } from './static/static.module';
-import {NgxPaginationModule} from 'ngx-pagination';
 
 
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PagesComponent } from './pages/pages.component';
+import { PagesModule } from './pages/pages.module';
 import { StaicComponent } from './static/staic.component';
 import { UserComponent } from './user/user/user.component';
-import { PagesModule } from './pages/pages.module';
 
 
 @NgModule({
@@ -36,6 +37,7 @@ import { PagesModule } from './pages/pages.module';
     PagesModule,
     HttpClientModule,
     NgxPaginationModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
